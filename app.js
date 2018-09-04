45aebd448571b02140b5b86c0f2f33c817f82be2
quilicicf <cquilici@restlet.com>
2018-09-04 21:23:31 +0200

    :up: New year, new rules

--- app.js
+++ app.js
@@ -1,22 +1,100 @@
-document.onkeydown = function(ev) {
-  var keyEvent = (ev || event);
-  var keyCode = keyEvent.keyCode;
-  if (keyCode === 116 || (keyCode === 82 && keyEvent.ctrlKey)) {
+document.onkeydown = function (event) {
+  if (event.key === 'F5' || (event.key.toLowerCase() === 'r' && event.ctrlKey)) {
+    console.log('Nope, cheater!');
     return false;  // disable F5 key and ctrl-r
   }
-}
 
-var words = [
-  'rouge',
-  'parallèle',
-  'onde électromagnétique',
-  'verrerie',
-  'slime',
-  'dioxygène',
-  '207'
+  if (event.altKey && event.ctrlKey && event.key === 'n') {
+    location.reload();
+    return false;
+  }
+};
+
+var computeScore = function (score, bonusMultiplier) {
+  return _.round(score * bonusMultiplier);
+};
+
+var MAX_WORD_SCORE = 100;
+var MAX_BONUS_MULTIPLIER = 2;
+var WORDS = [
+  {
+    value: 'rouge',
+    validate: function (proposition) {
+      return proposition === 'rouge' ? 100 : 0;
+    }
+  },
+  {
+    value: 'parallèle',
+    validate: function (proposition) {
+      if (proposition === 'parallèle') {
+        return 100;
+      }
+      return proposition === 'parallele' ? 80 : 0;
+    }
+  },
+  {
+    value: 'onde électro-magnétique',
+    validate: function (proposition) {
+      if (proposition === 'onde électro-magnétique') {
+        return 100;
+      }
+
+      if ([ 'onde electro-magnetique', 'onde électromagnétique' ].includes(proposition)) {
+        return 80;
+      }
+
+      return proposition === 'onde electromagnetique' ? 60 : 0;
+    }
+  },
+  {
+    value: 'verrerie',
+    validate: function (proposition) {
+      return proposition === 'verrerie' ? 100 : 0;
+    }
+  },
+  {
+    value: 'slime',
+    validate: function (proposition) {
+      return proposition === 'slime' ? 100 : 0;
+    }
+  },
+  {
+    value: 'dioxygène',
+    validate: function (proposition) {
+      if (proposition === 'dioxygène') {
+        return 100;
+      }
+      return proposition === 'dioxygene' ? 80 : 0;
+    }
+  },
+  {
+    value: '207',
+    validate: function (proposition) {
+      return proposition === '207' ? 100 : 0;
+    }
+  },
+  {
+    value: 'solide',
+    validate: function (proposition) {
+      return proposition === 'solide' ? 100 : 0;
+    }
+  },
+  {
+    value: 'réfraction',
+    validate: function (proposition) {
+      if (proposition === 'réfraction') {
+        return 100;
+      }
+      return proposition === 'refraction' ? 80 : 0;
+    }
+  }
 ];
 
-var inputCreator = function(index, word) {
+var MAX_WORDS_SCORE = _.size(WORDS) * MAX_WORD_SCORE;
+var MAX_TOTAL_SCORE = computeScore(MAX_WORDS_SCORE, MAX_BONUS_MULTIPLIER);
+
+
+var inputCreator = function (index) {
   return {
     $type: 'div',
     id: 'word' + index + '-container',
@@ -37,21 +115,38 @@ var inputCreator = function(index, word) {
   };
 };
 
-var processWord = function(word) {
-  var trimmed = _.trim(word);
-  return trimmed.toLowerCase();
-};
-
-var validateInput = function(input, container, word) {
-  var inputValue = input.value;
-  input.disabled = true;
-  if (processWord(input.value) === processWord(word)) {
-    container.classList.add('has-success');
-    return 100;
+var getBonusMultiplier = function (timeTaken) {
+  if (timeTaken < 30) {
+    return 2;
   }
 
-  container.classList.add('has-warning');
-  return 0;
+  if (timeTaken >= 80) {
+    return 0;
+  }
+
+  return 1 + ((80 - timeTaken) / 50);
+};
+
+var processWord = function (word) {
+  var trimmed = _.trim(word);
+  return _.toLower(trimmed);
+};
+
+var validateInput = function (input, container, word) {
+  input.disabled = true;
+  var score = word.validate(processWord(input.value));
+  if (score === 100) {
+    container.classList.add('has-success');
+
+  } else if (score === 0) {
+    container.classList.add('has-error');
+
+  } else {
+    container.classList.add('has-warning');
+
+  }
+
+  return score;
 };
 
 var startDate = new Date();
@@ -64,7 +159,8 @@ var el = {
     {
       $type: 'form',
       $components: [],
-      $init: function() {
+      autocomplete: 'off',
+      $init: function () {
         var index = 1;
 
         var components = this.$components;
@@ -75,11 +171,11 @@ var el = {
 
         components.push({
           $type: 'h2',
-          $text: 'Le jeu a commencé à ' + startHours + 'h' + startMinutes + '.'
+          $text: 'Le jeu a commencé à ' + _.padStart(startHours, 2, 0) + 'h' + _.padStart(startMinutes, 2, 0) + '.'
         });
 
-        _(words)
-          .each(function(word) {
+        _(WORDS)
+          .each(function (word) {
             components.push(inputCreator(index++, word));
           });
 
@@ -93,12 +189,12 @@ var el = {
           $type: 'button',
           class: 'btn btn-primary',
           $text: 'Valider',
-          onclick: function(event) {
+          onclick: function (event) {
             event.preventDefault();
             event.stopPropagation();
 
             var index = 0;
-            var score = _.reduce(words, function(currentScore, word) {
+            var score = _.reduce(WORDS, function (currentScore, word) {
               var container = document.querySelector('#word' + ++index + '-container');
               var input = document.querySelector('#word' + index + '-input');
 
@@ -114,28 +210,13 @@ var el = {
             var endTime = endHours * 60 + endMinutes;
 
             var timeTaken = endTime - startTime;
-            var bonus = 0;
-            if (timeTaken < 30) {
-              bonus = 500;
+            var bonusMultiplier = getBonusMultiplier(timeTaken);
+            var finalScore = _.round(score * bonusMultiplier);
 
-            } else if (timeTaken < 40) {
-              bonus = 250;
+            var firstSentence = 'Vous avez marqué ' + score + ' points pour vos réponses avec un bonus de temps de x' + bonusMultiplier + '.';
+            var secondSentence = 'Votre score final est de ' + finalScore + ' points (maximum possible de ' + MAX_TOTAL_SCORE + ').';
 
-            } else if (timeTaken < 50) {
-              bonus = 100;
-
-            } else if (timeTaken < 60) {
-              bonus = 50;
-
-            } else if (timeTaken < 70) {
-              bonus = 10;
-
-            }
-
-            var firstSentence = 'Vous avez marqué ' + score + ' points pour vos réponses plus ' + bonus + ' de temps.';
-            var secondSentence = 'Votre score final est de ' + (score + bonus) + ' points.';
-
-            result.innerHTML = '<p>' + _.join([firstSentence, secondSentence], '</p><p>') + '</p>';
+            result.innerHTML = '<p>' + _.join([ firstSentence, secondSentence ], '</p><p>') + '</p>';
           }
         });
       }
